955e03cade89f70053aa66e21942424b421b5004
Thomas van de Meer <t.vandemeer@gmail.com>
2016-12-11 20:20:58 +0100

    Changes to client

--- client.py
+++ client.py
@@ -6,19 +6,18 @@ import requests
 apikey = '14OGzuak'
 resptype = 'json'
 soort = 'schilderij'
-afbeelding = 'True'
+afbeelding = 'True'     # Alleen resultaten met afbeelding
 topstukken = 'True'
-aantal = 20
-
-# api endpoint voor topstukken uit collectie schilderijen
-endpoint = 'https://www.rijksmuseum.nl/api/nl/collection?key=%s&format=%s&type=%s&imgonly=%s&toppieces=%s&ps=%d' % (apikey, resptype, soort, afbeelding, topstukken, aantal)
-
-# api endpoint voor object info
-obj = 'https://www.rijksmuseum.nl/api/nl/collection/%s?key=%s&format=json'
+aantal = 20     # Aantal resultaten per pagina
 
 
 def request_stukken():
-    r = requests.get(endpoint)
+
+# api endpoint voor topstukken uit collectie schilderijen
+    col = 'https://www.rijksmuseum.nl/api/nl/collection?key=%s&format=%s&type=%s&imgonly=%s&toppieces=%s&ps=%d' % (
+        apikey, resptype, soort, afbeelding, topstukken, aantal)
+
+    r = requests.get(col)
     if r.status_code == 200:
         stukken = r.json()['artObjects']
         return stukken
@@ -26,6 +25,8 @@ def request_stukken():
         print('Er is een fout opgetreden')
 
 
-def object_info():
-    pass
+def object_info(object_id):
+
+    # api endpoint voor object info
+    obj = 'https://www.rijksmuseum.nl/api/nl/collection/%s?key=%s&format=json' % (object_id, apikey)
 
